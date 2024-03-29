4484047a09ee17170145f3ae0d0ebde2eaa6f0c4
BIngDiAn <32029400+BIngDiAn-s@users.noreply.github.com>
2024-03-29 21:10:33 +0800

    Update GlobalSerializeMapping.java

--- hutool-json/src/main/java/cn/hutool/json/serialize/GlobalSerializeMapping.java
+++ hutool-json/src/main/java/cn/hutool/json/serialize/GlobalSerializeMapping.java
@@ -11,8 +11,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * 全局的序列化和反序列化器映射<br>
- * 在JSON和Java对象转换过程中，优先使用注册于此处的自定义转换
  *
  * @author Looly
  *
@@ -40,30 +38,21 @@ public class GlobalSerializeMapping {
 	}
 
 	/**
-	 * 加入自定义的序列化器
-	 *
-	 * @param type 对象类型
-	 * @param serializer 序列化器实现
+	 * 
 	 */
 	public static void put(Type type, JSONArraySerializer<?> serializer) {
 		putInternal(type, serializer);
 	}
 
 	/**
-	 * 加入自定义的序列化器
-	 *
-	 * @param type 对象类型
-	 * @param serializer 序列化器实现
+	 * 
 	 */
 	public static void put(Type type, JSONObjectSerializer<?> serializer) {
 		putInternal(type, serializer);
 	}
 
 	/**
-	 * 加入自定义的序列化器
-	 *
-	 * @param type 对象类型
-	 * @param serializer 序列化器实现
+	
 	 */
 	synchronized private static void putInternal(Type type, JSONSerializer<? extends JSON, ?> serializer) {
 		if(null == serializerMap) {
@@ -73,10 +62,7 @@ public class GlobalSerializeMapping {
 	}
 
 	/**
-	 * 加入自定义的反序列化器
-	 *
-	 * @param type 对象类型
-	 * @param deserializer 反序列化器实现
+	 * 
 	 */
 	synchronized public static void put(Type type, JSONDeserializer<?> deserializer) {
 		if(null == deserializerMap) {
@@ -86,9 +72,6 @@ public class GlobalSerializeMapping {
 	}
 
 	/**
-	 * 获取自定义的序列化器，如果未定义返回{@code null}
-	 * @param type 类型
-	 * @return 自定义的序列化器或者{@code null}
 	 */
 	public static JSONSerializer<? extends JSON, ?> getSerializer(Type type){
 		if(null == serializerMap) {
@@ -98,9 +81,7 @@ public class GlobalSerializeMapping {
 	}
 
 	/**
-	 * 获取自定义的反序列化器，如果未定义返回{@code null}
-	 * @param type 类型
-	 * @return 自定义的反序列化器或者{@code null}
+	 * 
 	 */
 	public static JSONDeserializer<?> getDeserializer(Type type){
 		if(null == deserializerMap) {
