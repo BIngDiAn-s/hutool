8587b51e1c8d52f36024f097856beb1c59609e10
BIngDiAn <32029400+BIngDiAn-s@users.noreply.github.com>
2024-03-29 21:09:50 +0800

    Update ParseConfig.java

--- hutool-json/src/main/java/cn/hutool/json/xml/ParseConfig.java
+++ hutool-json/src/main/java/cn/hutool/json/xml/ParseConfig.java
@@ -15,21 +15,18 @@ package cn.hutool.json.xml;
 import java.io.Serializable;
 
 /**
- * XML解析为JSON的可选选项<br>
- * 参考：https://github.com/stleary/JSON-java/blob/master/src/main/java/org/json/ParserConfiguration.java
- *
- * @author AylwardJ, Looly
+ * 
  */
 public class ParseConfig implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 默认最大嵌套深度
+	 *
 	 */
 	public static final int DEFAULT_MAXIMUM_NESTING_DEPTH = 512;
 
 	/**
-	 * 创建ParseConfig
+	 * 
 	 *
 	 * @return ParseConfig
 	 */
@@ -38,28 +35,24 @@ public class ParseConfig implements Serializable {
 	}
 
 	/**
-	 * 是否保持值为String类型，如果为{@code false}，则尝试转换为对应类型(numeric, boolean, string)
+	 * 
 	 */
 	private boolean keepStrings;
 	/**
-	 * 最大嵌套深度，用于解析时限制解析层级，当大于这个层级时抛出异常，-1表示无限制
+	 * 
 	 */
 	private int maxNestingDepth = -1;
 
 	/**
-	 * 是否保持值为String类型，如果为{@code false}，则尝试转换为对应类型(numeric, boolean, string)
-	 *
-	 * @return 是否保持值为String类型
+	 * 
+	 * 
 	 */
 	public boolean isKeepStrings() {
 		return keepStrings;
 	}
 
 	/**
-	 * 设置是否保持值为String类型，如果为{@code false}，则尝试转换为对应类型(numeric, boolean, string)
-	 *
-	 * @param keepStrings 是否保持值为String类型
-	 * @return this
+	 * 
 	 */
 	public ParseConfig setKeepStrings(final boolean keepStrings) {
 		this.keepStrings = keepStrings;
@@ -67,19 +60,14 @@ public class ParseConfig implements Serializable {
 	}
 
 	/**
-	 * 获取最大嵌套深度，用于解析时限制解析层级，当大于这个层级时抛出异常，-1表示无限制
-	 *
-	 * @return 最大嵌套深度
+	 * 
 	 */
 	public int getMaxNestingDepth() {
 		return maxNestingDepth;
 	}
 
 	/**
-	 * 设置最大嵌套深度，用于解析时限制解析层级，当大于这个层级时抛出异常，-1表示无限制
-	 *
-	 * @param maxNestingDepth 最大嵌套深度
-	 * @return this
+	 * 
 	 */
 	public ParseConfig setMaxNestingDepth(final int maxNestingDepth) {
 		this.maxNestingDepth = maxNestingDepth;
